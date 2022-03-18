92c3b80fd871e2fd64a5501ae14d4da10bd5a3db
bot <abagoforgans@users.noreply.github.com>
2022-03-18 16:41:12 +0000

    autocommit

---
+++ contracts/0xe5eFf57F06B426612d91Eb8cc3880Ef2449E1B2D.sol
@@ -0,0 +1,39 @@
+contract main {
+
+
+
+
+// =====================  Runtime code  =====================
+
+
+#
+#  - convert(address arg1, address arg2)
+#
+address factoryAddress;
+address sub_3ecc52b9Address;
+address stormAddress;
+address wbnbAddress;
+
+function sub_3ecc52b9(?) payable {
+    return sub_3ecc52b9Address
+}
+
+function wbnb() payable {
+    return wbnbAddress
+}
+
+function storm() payable {
+    return stormAddress
+}
+
+function factory() payable {
+    return factoryAddress
+}
+
+function _fallback() payable {
+    revert
+}
+
+
+
+}
