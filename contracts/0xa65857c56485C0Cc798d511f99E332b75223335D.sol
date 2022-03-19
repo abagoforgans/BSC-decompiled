724edd437513d3fa3544ff55c12ce27bc986af84
bot <abagoforgans@users.noreply.github.com>
2022-03-19 13:04:26 +0000

    autocommit

---
+++ contracts/0xa65857c56485C0Cc798d511f99E332b75223335D.sol
@@ -0,0 +1,88 @@
+contract main {
+
+
+
+
+// =====================  Runtime code  =====================
+
+
+#
+#  - claim(address arg1, bytes32 arg2)
+#  - sub_cb2f03b4(?)
+#
+mapping of uint256 balanceOf;
+mapping of uint8 claims;
+array of uint256 name;
+array of uint256 symbol;
+uint8 decimals;
+uint256 totalSupply;
+address bossAddress;
+mapping of uint256 allowance;
+
+function name() {
+    return name[0 len name.length]
+}
+
+function totalSupply() {
+    return totalSupply
+}
+
+function decimals() {
+    return decimals
+}
+
+function balanceOf(address arg1) {
+    return balanceOf[arg1]
+}
+
+function symbol() {
+    return symbol[0 len symbol.length]
+}
+
+function boss() {
+    return bossAddress
+}
+
+function allowance(address arg1, address arg2) {
+    return allowance[arg1][arg2]
+}
+
+function claims(bytes32 arg1) {
+    return claims[arg1]
+}
+
+function approve(address arg1, uint256 arg2) {
+    allowance[address(msg.sender)][address(arg1)] = arg2
+    emit Approval(arg2, msg.sender, arg1);
+    return 1
+}
+
+function _fallback() payable {
+    call bossAddress with:
+       value msg.value wei
+         gas 2300 * is_zero(value) wei
+    if not ext_call.success:
+        revert with ext_call.return_data[0 len return_data.size]
+}
+
+function transfer(address arg1, uint256 arg2) {
+    require balanceOf[address(msg.sender)] >= arg2
+    balanceOf[address(msg.sender)] -= arg2
+    balanceOf[address(arg1)] += arg2
+    emit Transfer(arg2, msg.sender, arg1);
+    return 1
+}
+
+function transferFrom(address arg1, address arg2, uint256 arg3) {
+    require arg3 <= balanceOf[address(arg1)]
+    require arg3 <= allowance[address(arg1)][address(msg.sender)]
+    balanceOf[address(arg1)] -= arg3
+    balanceOf[address(arg2)] += arg3
+    allowance[address(arg1)][address(msg.sender)] -= arg3
+    emit Transfer(arg3, arg1, arg2);
+    return 1
+}
+
+
+
+}
