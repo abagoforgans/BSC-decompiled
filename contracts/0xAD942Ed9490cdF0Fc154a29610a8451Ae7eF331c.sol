12e5634176fb8ab1da0d1c98a4105bf275d9fc65
bot <abagoforgans@users.noreply.github.com>
2022-03-21 14:27:04 +0000

    autocommit

---
+++ contracts/0xAD942Ed9490cdF0Fc154a29610a8451Ae7eF331c.sol
@@ -0,0 +1,22 @@
+contract main {
+
+
+
+
+// =====================  Runtime code  =====================
+
+
+function _fallback() payable {
+  stop
+}
+
+function sub_4a36e47b(?) {
+    require calldata.size - 4 >= 64
+    require arg1 == address(arg1)
+    require arg2 != address(arg2)
+    revert
+}
+
+
+
+}
