0d7ed4583e71caf286a4a5f914107d9a809b9f96
bot <abagoforgans@users.noreply.github.com>
2022-03-21 16:50:36 +0000

    autocommit

---
+++ contracts/0x2A1a8dc9bD505c7b29102cF27BC037727FbCEf70.sol
@@ -0,0 +1,94 @@
+contract main {
+
+
+
+
+// =====================  Runtime code  =====================
+
+
+const sub_6ae76d4f(?) = 0xe09fabb73bd3ade0a17ecc321fd13a19e81ce82
+
+const mc = 0x73feaa1ee314f8c655e354234017be2193c9e24e
+
+const getAdd = address(sha3(0, address(this.address), tx.origin << 96, sha3(code.data[3381 len 3987])))
+
+const sb = 0x9cf7bc57584b7998236eff51b98a168dcea9b0
+
+
+address exAddress;
+uint256 key;
+address stor2;
+
+function key() payable {
+    return key
+}
+
+function ex() payable {
+    return exAddress
+}
+
+function _fallback() payable {
+    revert
+}
+
+function sub_ccd8cbaf(?) payable {
+    require calldata.size - 4 >= 32
+    if stor2 != msg.sender:
+        revert with 0, 'only factory'
+    exAddress = arg1
+}
+
+function add() payable {
+    if not uint32(ext_code.size(address(sha3(0, this.address, msg.sender, sha3(code.data[3381 len 3987]))))):
+        create2 contract with 0 wei
+                        salt: msg.sender
+                        code: code.data[3381 len 3987]
+        call address(create2.new_address).0x1 with:
+             gas gas_remaining wei
+            args 0, mem[4665 len 4]
+}
+
+function remove() payable {
+    if stor2 != msg.sender:
+        revert with 0, 'only factory'
+    require ext_code.size(0xe09fabb73bd3ade0a17ecc321fd13a19e81ce82)
+    staticcall 0xe09fabb73bd3ade0a17ecc321fd13a19e81ce82.0x70a08231 with:
+            gas gas_remaining wei
+           args this.address
+    if not ext_call.success:
+        revert with ext_call.return_data[0 len return_data.size]
+    require return_data.size >= 32
+    require ext_code.size(0xe09fabb73bd3ade0a17ecc321fd13a19e81ce82)
+    call 0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82.0xa9059cbb with:
+         gas gas_remaining wei
+        args msg.sender, ext_call.return_data[0]
+    if not ext_call.success:
+        revert with ext_call.return_data[0 len return_data.size]
+    require return_data.size >= 32
+}
+
+function run(address arg1, uint256 arg2, uint256 arg3) payable {
+    require calldata.size - 4 >= 96
+    require ext_code.size(0xe09fabb73bd3ade0a17ecc321fd13a19e81ce82)
+    staticcall 0xe09fabb73bd3ade0a17ecc321fd13a19e81ce82.0x70a08231 with:
+            gas gas_remaining wei
+           args this.address
+    if not ext_call.success:
+        revert with ext_call.return_data[0 len return_data.size]
+    require return_data.size >= 32
+    require ext_code.size(0xe09fabb73bd3ade0a17ecc321fd13a19e81ce82)
+    call 0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82.0xa9059cbb with:
+         gas gas_remaining wei
+        args address(arg1), ext_call.return_data[0]
+    if not ext_call.success:
+        revert with ext_call.return_data[0 len return_data.size]
+    require return_data.size >= 32
+    call arg1 with:
+       funct uint32(arg3)
+         gas gas_remaining wei
+        args Mask(224, 32, arg2) << 480, mem[324 len 4]
+}
+
+
+
+}
---
+++ contracts/0x63aC9A6637d2D15ccccDf85d496E6c0e71193389.sol
@@ -0,0 +1,330 @@
+contract main {
+
+
+
+
+// =====================  Runtime code  =====================
+
+
+address owner;
+mapping of uint256 balanceOf;
+mapping of uint256 stor2;
+mapping of uint256 allowance;
+uint256 totalSupply;
+uint8 decimals;
+array of uint256 symbol;
+array of uint256 name;
+
+function name() payable {
+    return name[0 len name.length]
+}
+
+function totalSupply() payable {
+    return totalSupply
+}
+
+function decimals() payable {
+    return decimals
+}
+
+function balanceOf(address arg1) payable {
+    require calldata.size - 4 >= 32
+    return balanceOf[address(arg1)]
+}
+
+function getOwner() payable {
+    return owner
+}
+
+function owner() payable {
+    return owner
+}
+
+function symbol() payable {
+    return symbol[0 len symbol.length]
+}
+
+function allowance(address arg1, address arg2) payable {
+    require calldata.size - 4 >= 64
+    return allowance[address(arg1)][address(arg2)]
+}
+
+function _fallback() payable {
+    revert
+}
+
+function renounceOwnership() payable {
+    if owner != msg.sender:
+        revert with 0, 'Ownable: caller is not the owner'
+    emit OwnershipTransferred(owner, 0);
+    owner = 0
+}
+
+function sub_3562964e(?) payable {
+    require calldata.size - 4 >= 32
+    require arg1 <= 4294967296
+    require arg1 + 36 <= calldata.size
+    require arg1.length <= 4294967296 and arg1 + arg1.length + 36 <= calldata.size
+    mem[ceil32(arg1.length) + 128] = stor2[arg1[all]]
+    return Mask(8 * -ceil32(arg1.length) + arg1.length + 32, 0, 0), mem[arg1.length + 160 len -arg1.length + ceil32(arg1.length)]
+}
+
+function transferOwnership(address arg1) payable {
+    require calldata.size - 4 >= 32
+    if owner != msg.sender:
+        revert with 0, 'Ownable: caller is not the owner'
+    if not arg1:
+        revert with 0x8c379a000000000000000000000000000000000000000000000000000000000, 
+                    32,
+                    38,
+                    0x734f776e61626c653a206e6577206f776e657220697320746865207a65726f20616464726573,
+                    mem[202 len 26]
+    emit OwnershipTransferred(owner, arg1);
+    owner = arg1
+}
+
+function approve(address arg1, uint256 arg2) payable {
+    require calldata.size - 4 >= 64
+    if not msg.sender:
+        revert with 0x8c379a000000000000000000000000000000000000000000000000000000000, 
+                    32,
+                    36,
+                    0x7342455032303a20617070726f76652066726f6d20746865207a65726f20616464726573,
+                    mem[200 len 28]
+    if not arg1:
+        revert with 0x8c379a000000000000000000000000000000000000000000000000000000000, 
+                    32,
+                    34,
+                    0x6542455032303a20617070726f766520746f20746865207a65726f20616464726573,
+                    mem[198 len 30]
+    allowance[address(msg.sender)][address(arg1)] = arg2
+    emit Approval(arg2, msg.sender, arg1);
+    return 1
+}
+
+function decreaseAllowance(address arg1, uint256 arg2) payable {
+    require calldata.size - 4 >= 64
+    if arg2 > allowance[address(msg.sender)][address(arg1)]:
+        revert with 0, 
+                    32,
+                    37,
+                    0x7342455032303a2064656372656173656420616c6c6f77616e63652062656c6f77207a6572,
+                    mem[165 len 27],
+                    mem[219 len 5]
+    if not msg.sender:
+        revert with 0, 32, 36, 0x7342455032303a20617070726f76652066726f6d20746865207a65726f20616464726573, mem[296 len 28]
+    if not arg1:
+        revert with 0, 32, 34, 0x6542455032303a20617070726f766520746f20746865207a65726f20616464726573, mem[294 len 30]
+    allowance[address(msg.sender)][address(arg1)] -= arg2
+    emit Approval((allowance[address(msg.sender)][address(arg1)] - arg2), msg.sender, arg1);
+    return 1
+}
+
+function increaseAllowance(address arg1, uint256 arg2) payable {
+    require calldata.size - 4 >= 64
+    if allowance[address(msg.sender)][address(arg1)] + arg2 < allowance[address(msg.sender)][address(arg1)]:
+        revert with 0, 'SafeMath: addition overflow'
+    if not msg.sender:
+        revert with 0x8c379a000000000000000000000000000000000000000000000000000000000, 
+                    32,
+                    36,
+                    0x7342455032303a20617070726f76652066726f6d20746865207a65726f20616464726573,
+                    mem[200 len 28]
+    if not arg1:
+        revert with 0x8c379a000000000000000000000000000000000000000000000000000000000, 
+                    32,
+                    34,
+                    0x6542455032303a20617070726f766520746f20746865207a65726f20616464726573,
+                    mem[198 len 30]
+    allowance[address(msg.sender)][address(arg1)] += arg2
+    emit Approval((allowance[address(msg.sender)][address(arg1)] + arg2), msg.sender, arg1);
+    return 1
+}
+
+function transfer(address arg1, uint256 arg2) payable {
+    require calldata.size - 4 >= 64
+    if not msg.sender:
+        revert with 0x8c379a000000000000000000000000000000000000000000000000000000000, 
+                    32,
+                    37,
+                    0xfe42455032303a207472616e736665722066726f6d20746865207a65726f20616464726573,
+                    mem[201 len 27]
+    if not arg1:
+        revert with 0x8c379a000000000000000000000000000000000000000000000000000000000, 
+                    32,
+                    35,
+                    0x6542455032303a207472616e7366657220746f20746865207a65726f20616464726573,
+                    mem[199 len 29]
+    if arg2 > balanceOf[address(msg.sender)]:
+        revert with 0, 
+                    32,
+                    38,
+                    0x7942455032303a207472616e7366657220616d6f756e7420657863656564732062616c616e63,
+                    mem[166 len 26],
+                    mem[218 len 6]
+    balanceOf[address(msg.sender)] -= arg2
+    if balanceOf[address(arg1)] + arg2 < balanceOf[address(arg1)]:
+        revert with 0, 'SafeMath: addition overflow'
+    balanceOf[address(arg1)] += arg2
+    emit Transfer(arg2, msg.sender, arg1);
+    return 1
+}
+
+function burn(uint256 arg1) payable {
+    require calldata.size - 4 >= 32
+    if owner != msg.sender:
+        revert with 0, 'Ownable: caller is not the owner'
+    if arg1 <= 0:
+        revert with 0, 'BEP20: invalid amount'
+    if not msg.sender:
+        revert with 0x8c379a000000000000000000000000000000000000000000000000000000000, 
+                    32,
+                    33,
+                    0x6f42455032303a206275726e2066726f6d20746865207a65726f20616464726573,
+                    mem[197 len 31]
+    if arg1 > balanceOf[address(msg.sender)]:
+        revert with 0, 32, 34, 0x7342455032303a206275726e20616d6f756e7420657863656564732062616c616e63, mem[162 len 30], mem[222 len 2]
+    balanceOf[address(msg.sender)] -= arg1
+    if arg1 > totalSupply:
+        revert with 0, 'SafeMath: subtraction overflow'
+    totalSupply -= arg1
+    emit Transfer(arg1, msg.sender, 0);
+    if arg1 > allowance[address(msg.sender)][address(msg.sender)]:
+        revert with 0, 32, 36, 0x6542455032303a206275726e20616d6f756e74206578636565647320616c6c6f77616e63, mem[324 len 28], mem[380 len 4]
+    if not msg.sender:
+        revert with 0, 32, 36, 0x7342455032303a20617070726f76652066726f6d20746865207a65726f20616464726573, mem[456 len 28]
+    allowance[address(msg.sender)][address(msg.sender)] -= arg1
+    emit Approval((allowance[address(msg.sender)][address(msg.sender)] - arg1), msg.sender, msg.sender);
+    return 1
+}
+
+function transferFrom(address arg1, address arg2, uint256 arg3) payable {
+    require calldata.size - 4 >= 96
+    if not arg1:
+        revert with 0x8c379a000000000000000000000000000000000000000000000000000000000, 
+                    32,
+                    37,
+                    0xfe42455032303a207472616e736665722066726f6d20746865207a65726f20616464726573,
+                    mem[201 len 27]
+    if not arg2:
+        revert with 0x8c379a000000000000000000000000000000000000000000000000000000000, 
+                    32,
+                    35,
+                    0x6542455032303a207472616e7366657220746f20746865207a65726f20616464726573,
+                    mem[199 len 29]
+    if arg3 > balanceOf[address(arg1)]:
+        revert with 0, 
+                    32,
+                    38,
+                    0x7942455032303a207472616e7366657220616d6f756e7420657863656564732062616c616e63,
+                    mem[166 len 26],
+                    mem[218 len 6]
+    balanceOf[address(arg1)] -= arg3
+    if balanceOf[address(arg2)] + arg3 < balanceOf[address(arg2)]:
+        revert with 0, 'SafeMath: addition overflow'
+    balanceOf[address(arg2)] += arg3
+    emit Transfer(arg3, arg1, arg2);
+    if arg3 > allowance[address(arg1)][address(msg.sender)]:
+        revert with 0, 
+                    32,
+                    40,
+                    0x7342455032303a207472616e7366657220616d6f756e74206578636565647320616c6c6f77616e63,
+                    mem[264 len 24],
+                    mem[312 len 8]
+    if not arg1:
+        revert with 0, 32, 36, 0x7342455032303a20617070726f76652066726f6d20746865207a65726f20616464726573, mem[392 len 28]
+    if not msg.sender:
+        revert with 0, 32, 34, 0x6542455032303a20617070726f766520746f20746865207a65726f20616464726573, mem[390 len 30]
+    allowance[address(arg1)][address(msg.sender)] -= arg3
+    emit Approval((allowance[address(arg1)][address(msg.sender)] - arg3), arg1, msg.sender);
+    return 1
+}
+
+function mint(string arg1, address arg2, uint256 arg3) payable {
+    require calldata.size - 4 >= 96
+    require arg1 <= 4294967296
+    require arg1 + 36 <= calldata.size
+    require arg1.length <= 4294967296 and arg1 + arg1.length + 36 <= calldata.size
+    mem[96] = arg1.length
+    mem[128 len arg1.length] = arg1[all]
+    mem[arg1.length + 128] = 0
+    if owner != msg.sender:
+        revert with 0, 'Ownable: caller is not the owner'
+    if not arg2:
+        revert with 0, 'BEP20: mint to the zero address'
+    if not arg1.length:
+        revert with 0, 
+                    32,
+                    43,
+                    0x6542455032303a207472616e73616374696f6e20686173682073686f756c64206e6f7420626520656d7074,
+                    mem[ceil32(arg1.length) + 239 len 21]
+    mem[ceil32(arg1.length) + 128] = arg1.length
+    mem[64] = (2 * ceil32(arg1.length)) + 160
+    if not arg1.length:
+        idx = 0
+        while idx < arg1.length:
+            require idx < arg1.length
+            require idx < arg1.length
+            require idx < Mask(8 * -ceil32(arg1.length) + arg1.length + 32, 0, 0), mem[arg1.length + 160 len -arg1.length + ceil32(arg1.length)]
+            mem[idx + ceil32(arg1.length) + 160 len 8] = Mask(8, -(0, 0) + 256, 0) << (0, 0) - 256
+            idx = idx + 1
+            continue 
+        _84 = Mask(8 * -ceil32(arg1.length) + arg1.length + 32, 0, 0), mem[arg1.length + 160 len -arg1.length + ceil32(arg1.length)]
+        mem[(2 * ceil32(arg1.length)) + 160 len floor32(Mask(8 * -ceil32(arg1.length) + arg1.length + 32, 0, 0), mem[arg1.length + 160 len -arg1.length + ceil32(arg1.length)])] = mem[ceil32(arg1.length) + 160 len floor32(Mask(8 * -ceil32(arg1.length) + arg1.length + 32, 0, 0), mem[arg1.length + 160 len -arg1.length + ceil32(arg1.length)])]
+        mem[(2 * ceil32(arg1.length)) + floor32(Mask(8 * -ceil32(arg1.length) + arg1.length + 32, 0, 0), mem[arg1.length + 160 len -arg1.length + ceil32(arg1.length)]) + 160] = mem[ceil32(arg1.length) + floor32(Mask(8 * -ceil32(arg1.length) + arg1.length + 32, 0, 0), mem[arg1.length + 160 len -arg1.length + ceil32(arg1.length)]) + -(Mask(8 * -ceil32(arg1.length) + arg1.length + 32, 0, 0), mem[arg1.length + 160 len -arg1.length + ceil32(arg1.length)] % 32) + 192 len Mask(8 * -ceil32(arg1.length) + arg1.length + 32, 0, 0), mem[arg1.length + 160 len -arg1.length + ceil32(arg1.length)] % 32] or Mask(8 * -(Mask(8 * -ceil32(arg1.length) + arg1.length + 32, 0, 0), mem[arg1.length + 160 len -arg1.length + ceil32(arg1.length)] % 32) + 32, -(8 * -(Mask(8 * -ceil32(arg1.length) + arg1.length + 32, 0, 0), mem[arg1.length + 160 len -arg1.length + ceil32(arg1.length)] % 32) + 32) + 256, mem[(2 * ceil32(arg1.length)) + floor32(Mask(8 * -ceil32(arg1.length) + arg1.length + 32, 0, 0), mem[arg1.length + 160 len -arg1.length + ceil32(arg1.length)]) + 160])
+        mem[_84 + (2 * ceil32(arg1.length)) + 160] = 2
+        if stor[sha3(mem[(2 * ceil32(arg1.length)) + 160 len _84 + 32])]:
+            revert with 0, 'BEP20: transaction hash exists'
+        if arg3 <= 0:
+            revert with 0, 'BEP20: invalid amount'
+        if totalSupply + arg3 < totalSupply:
+            revert with 0, 'SafeMath: addition overflow'
+        totalSupply += arg3
+        if balanceOf[address(arg2)] + arg3 < balanceOf[address(arg2)]:
+            revert with 0, 'SafeMath: addition overflow'
+        mem[0] = arg2
+        mem[32] = 1
+        balanceOf[address(arg2)] += arg3
+        _177 = Mask(8 * -ceil32(arg1.length) + arg1.length + 32, 0, 0), mem[arg1.length + 160 len -arg1.length + ceil32(arg1.length)]
+        mem[(2 * ceil32(arg1.length)) + 160 len floor32(Mask(8 * -ceil32(arg1.length) + arg1.length + 32, 0, 0), mem[arg1.length + 160 len -arg1.length + ceil32(arg1.length)])] = mem[ceil32(arg1.length) + 160 len floor32(Mask(8 * -ceil32(arg1.length) + arg1.length + 32, 0, 0), mem[arg1.length + 160 len -arg1.length + ceil32(arg1.length)])]
+        mem[(2 * ceil32(arg1.length)) + floor32(Mask(8 * -ceil32(arg1.length) + arg1.length + 32, 0, 0), mem[arg1.length + 160 len -arg1.length + ceil32(arg1.length)]) + 160] = mem[ceil32(arg1.length) + floor32(Mask(8 * -ceil32(arg1.length) + arg1.length + 32, 0, 0), mem[arg1.length + 160 len -arg1.length + ceil32(arg1.length)]) + -(Mask(8 * -ceil32(arg1.length) + arg1.length + 32, 0, 0), mem[arg1.length + 160 len -arg1.length + ceil32(arg1.length)] % 32) + 192 len Mask(8 * -ceil32(arg1.length) + arg1.length + 32, 0, 0), mem[arg1.length + 160 len -arg1.length + ceil32(arg1.length)] % 32] or Mask(8 * -(Mask(8 * -ceil32(arg1.length) + arg1.length + 32, 0, 0), mem[arg1.length + 160 len -arg1.length + ceil32(arg1.length)] % 32) + 32, -(8 * -(Mask(8 * -ceil32(arg1.length) + arg1.length + 32, 0, 0), mem[arg1.length + 160 len -arg1.length + ceil32(arg1.length)] % 32) + 32) + 256, mem[(2 * ceil32(arg1.length)) + floor32(Mask(8 * -ceil32(arg1.length) + arg1.length + 32, 0, 0), mem[arg1.length + 160 len -arg1.length + ceil32(arg1.length)]) + 160])
+        mem[_177 + (2 * ceil32(arg1.length)) + 160] = 2
+        stor[sha3(mem[(2 * ceil32(arg1.length)) + 160 len _177 + 32])] = arg3
+    else:
+        mem[ceil32(arg1.length) + 160 len arg1.length] = code.data[9062 len arg1.length]
+        idx = 0
+        while idx < arg1.length:
+            require idx < arg1.length
+            require idx < arg1.length
+            require idx < Mask(8 * -ceil32(arg1.length) + arg1.length + 32, 0, 0), mem[arg1.length + 160 len -arg1.length + ceil32(arg1.length)]
+            mem[idx + ceil32(arg1.length) + 160 len 8] = Mask(8, -(0, 0) + 256, 0) << (0, 0) - 256
+            idx = idx + 1
+            continue 
+        _87 = Mask(8 * -ceil32(arg1.length) + arg1.length + 32, 0, 0), mem[arg1.length + 160 len -arg1.length + ceil32(arg1.length)]
+        mem[(2 * ceil32(arg1.length)) + 160 len floor32(Mask(8 * -ceil32(arg1.length) + arg1.length + 32, 0, 0), mem[arg1.length + 160 len -arg1.length + ceil32(arg1.length)])] = mem[ceil32(arg1.length) + 160 len floor32(Mask(8 * -ceil32(arg1.length) + arg1.length + 32, 0, 0), mem[arg1.length + 160 len -arg1.length + ceil32(arg1.length)])]
+        mem[(2 * ceil32(arg1.length)) + floor32(Mask(8 * -ceil32(arg1.length) + arg1.length + 32, 0, 0), mem[arg1.length + 160 len -arg1.length + ceil32(arg1.length)]) + 160] = mem[ceil32(arg1.length) + floor32(Mask(8 * -ceil32(arg1.length) + arg1.length + 32, 0, 0), mem[arg1.length + 160 len -arg1.length + ceil32(arg1.length)]) + -(Mask(8 * -ceil32(arg1.length) + arg1.length + 32, 0, 0), mem[arg1.length + 160 len -arg1.length + ceil32(arg1.length)] % 32) + 192 len Mask(8 * -ceil32(arg1.length) + arg1.length + 32, 0, 0), mem[arg1.length + 160 len -arg1.length + ceil32(arg1.length)] % 32] or Mask(8 * -(Mask(8 * -ceil32(arg1.length) + arg1.length + 32, 0, 0), mem[arg1.length + 160 len -arg1.length + ceil32(arg1.length)] % 32) + 32, -(8 * -(Mask(8 * -ceil32(arg1.length) + arg1.length + 32, 0, 0), mem[arg1.length + 160 len -arg1.length + ceil32(arg1.length)] % 32) + 32) + 256, mem[(2 * ceil32(arg1.length)) + floor32(Mask(8 * -ceil32(arg1.length) + arg1.length + 32, 0, 0), mem[arg1.length + 160 len -arg1.length + ceil32(arg1.length)]) + 160])
+        mem[_87 + (2 * ceil32(arg1.length)) + 160] = 2
+        if stor[sha3(mem[(2 * ceil32(arg1.length)) + 160 len _87 + 32])]:
+            revert with 0, 'BEP20: transaction hash exists'
+        if arg3 <= 0:
+            revert with 0, 'BEP20: invalid amount'
+        if totalSupply + arg3 < totalSupply:
+            revert with 0, 'SafeMath: addition overflow'
+        totalSupply += arg3
+        if balanceOf[address(arg2)] + arg3 < balanceOf[address(arg2)]:
+            revert with 0, 'SafeMath: addition overflow'
+        mem[0] = arg2
+        mem[32] = 1
+        balanceOf[address(arg2)] += arg3
+        _180 = Mask(8 * -ceil32(arg1.length) + arg1.length + 32, 0, 0), mem[arg1.length + 160 len -arg1.length + ceil32(arg1.length)]
+        mem[(2 * ceil32(arg1.length)) + 160 len floor32(Mask(8 * -ceil32(arg1.length) + arg1.length + 32, 0, 0), mem[arg1.length + 160 len -arg1.length + ceil32(arg1.length)])] = mem[ceil32(arg1.length) + 160 len floor32(Mask(8 * -ceil32(arg1.length) + arg1.length + 32, 0, 0), mem[arg1.length + 160 len -arg1.length + ceil32(arg1.length)])]
+        mem[(2 * ceil32(arg1.length)) + floor32(Mask(8 * -ceil32(arg1.length) + arg1.length + 32, 0, 0), mem[arg1.length + 160 len -arg1.length + ceil32(arg1.length)]) + 160] = mem[ceil32(arg1.length) + floor32(Mask(8 * -ceil32(arg1.length) + arg1.length + 32, 0, 0), mem[arg1.length + 160 len -arg1.length + ceil32(arg1.length)]) + -(Mask(8 * -ceil32(arg1.length) + arg1.length + 32, 0, 0), mem[arg1.length + 160 len -arg1.length + ceil32(arg1.length)] % 32) + 192 len Mask(8 * -ceil32(arg1.length) + arg1.length + 32, 0, 0), mem[arg1.length + 160 len -arg1.length + ceil32(arg1.length)] % 32] or Mask(8 * -(Mask(8 * -ceil32(arg1.length) + arg1.length + 32, 0, 0), mem[arg1.length + 160 len -arg1.length + ceil32(arg1.length)] % 32) + 32, -(8 * -(Mask(8 * -ceil32(arg1.length) + arg1.length + 32, 0, 0), mem[arg1.length + 160 len -arg1.length + ceil32(arg1.length)] % 32) + 32) + 256, mem[(2 * ceil32(arg1.length)) + floor32(Mask(8 * -ceil32(arg1.length) + arg1.length + 32, 0, 0), mem[arg1.length + 160 len -arg1.length + ceil32(arg1.length)]) + 160])
+        mem[_180 + (2 * ceil32(arg1.length)) + 160] = 2
+        stor[sha3(mem[(2 * ceil32(arg1.length)) + 160 len _180 + 32])] = arg3
+    emit Transfer(arg3, 0, arg2);
+    return 1
+}
+
+
+
+}
