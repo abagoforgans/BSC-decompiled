4a5aaaf3c864cc5cd16f5733c3734a265079d537
bot <abagoforgans@users.noreply.github.com>
2022-03-22 06:10:00 +0000

    autocommit

---
+++ contracts/0xc3b45185490a2CC89a190937084445B049eA0552.sol
@@ -0,0 +1,48 @@
+contract main {
+
+
+
+
+// =====================  Runtime code  =====================
+
+
+#
+#  - flash(address arg1, address arg2, uint256 arg3, uint256 arg4, uint256 arg5, uint256 arg6)
+#
+address pancakeRouterAddress;
+address sub_cadfc995Address;
+address bakeryRouterAddress;
+address sub_6bd46787Address;
+address _owner;
+
+function sub_6bd46787(?) {
+    return sub_6bd46787Address
+}
+
+function owner() {
+    return _owner
+}
+
+function _owner() {
+    return _owner
+}
+
+function pancakeRouter() {
+    return pancakeRouterAddress
+}
+
+function sub_cadfc995(?) {
+    return sub_cadfc995Address
+}
+
+function bakeryRouter() {
+    return bakeryRouterAddress
+}
+
+function _fallback() payable {
+    revert
+}
+
+
+
+}
