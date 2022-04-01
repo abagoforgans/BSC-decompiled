f2ebb744b4c6e2f3c6174dd74706aadd972b1fb5
bot <abagoforgans@users.noreply.github.com>
2022-04-01 17:27:17 +0000

    autocommit

---
+++ contracts/0xCF6A7CC99bbbc03fCf37Ad41d47dE9E48d453143.sol
@@ -0,0 +1,173 @@
+contract main {
+
+
+
+
+// =====================  Runtime code  =====================
+
+
+address owner;
+uint32 stor1;
+address stakingPoolAddress;
+uint256 stor1;
+address rewardTokenAddress;
+
+function stakingPool() payable {
+    return address(stakingPoolAddress)
+}
+
+function owner() payable {
+    return owner
+}
+
+function rewardToken() payable {
+    return rewardTokenAddress
+}
+
+function _fallback() payable {
+    revert
+}
+
+function renounceOwnership() payable {
+    if owner != msg.sender:
+        revert with 0, 'Ownable: caller is not the owner'
+    emit OwnershipTransferred(owner, 0);
+    owner = 0
+}
+
+function setRewardToken(address arg1) payable {
+    require calldata.size - 4 >= 32
+    if owner != msg.sender:
+        revert with 0, 'Ownable: caller is not the owner'
+    rewardTokenAddress = arg1
+}
+
+function setStakingPool(address arg1) payable {
+    require calldata.size - 4 >= 32
+    if owner != msg.sender:
+        revert with 0, 'Ownable: caller is not the owner'
+    address(stakingPoolAddress) = arg1
+}
+
+function transferOwnership(address arg1) payable {
+    require calldata.size - 4 >= 32
+    if owner != msg.sender:
+        revert with 0, 'Ownable: caller is not the owner'
+    if not arg1:
+        revert with 0x8c379a000000000000000000000000000000000000000000000000000000000, 
+                    32,
+                    38,
+                    0xfe4f776e61626c653a206e6577206f776e657220697320746865207a65726f20616464726573,
+                    mem[202 len 26]
+    emit OwnershipTransferred(owner, arg1);
+    owner = arg1
+}
+
+function transferOut(address arg1, address arg2, uint256 arg3) payable {
+    require calldata.size - 4 >= 96
+    if owner != msg.sender:
+        revert with 0, 'Ownable: caller is not the owner'
+    if eth.balance(this.address) < 0:
+        revert with 0, 32, 38, 0x73416464726573733a20696e73756666696369656e742062616c616e636520666f722063616c, mem[366 len 26]
+    if ext_code.size(arg1) <= 0:
+        revert with 0, 'Address: call to non-contract'
+    mem[260 len 64] = unknown_0xa9059cbb(?????), address(arg2) << 64, 0, Mask(224, 32, arg3) >> 32
+    call arg1 with:
+         gas gas_remaining wei
+        args Mask(224, 32, arg3) << 224, mem[324 len 4]
+    if not return_data.size:
+        if not ext_call.success:
+            revert with unknown_0xa9059cbb(?????), address(arg2) << 64, 0, arg3
+        if not unknown_0xa9059cbb(?????), address(arg2) << 64:
+            revert with 0, 32, 42, 0x6c5361666545524332303a204552433230206f7065726174696f6e20646964206e6f7420737563636565, mem[370 len 22]
+    else:
+        mem[292 len return_data.size] = ext_call.return_data[0 len return_data.size]
+        if not ext_call.success:
+            if return_data.size > 0:
+                revert with ext_call.return_data[0 len return_data.size]
+            revert with 0, 'SafeERC20: low-level call failed'
+        if return_data.size > 0:
+            require return_data.size >= 32
+            if not mem[292]:
+                revert with 0, 
+                            32,
+                            42,
+                            0x6c5361666545524332303a204552433230206f7065726174696f6e20646964206e6f7420737563636565,
+                            mem[ceil32(return_data.size) + 371 len 22]
+}
+
+function sub_d20b08fb(?) payable {
+    require calldata.size - 4 >= 96
+    require arg3 <= 4294967296
+    require arg3 + 36 <= calldata.size
+    require arg3.length <= 4294967296 and arg3 + (32 * arg3.length) + 36 <= calldata.size
+    if owner != msg.sender:
+        revert with 0, 'Ownable: caller is not the owner'
+    if block.timestamp + 600 < block.timestamp:
+        revert with 0, 'SafeMath: addition overflow'
+    mem[96] = 0x38ed173900000000000000000000000000000000000000000000000000000000
+    mem[100] = arg2
+    mem[132] = 0
+    mem[196] = this.address
+    mem[228] = block.timestamp + 600
+    mem[164] = 160
+    mem[260] = arg3.length
+    mem[292 len 32 * arg3.length] = call.data[arg3 + 36 len 32 * arg3.length]
+    mem[(32 * arg3.length) + 292] = 0
+    require ext_code.size(arg1)
+    call arg1.swapExactTokensForTokens(uint256 rg1, uint256 rg2, address[] rg3, address rg4, uint256 rg5) with:
+         gas gas_remaining wei
+        args 0, uint32(arg2), 0, 160, address(this.address), block.timestamp + 600, arg3.length, call.data[arg3 + 36 len floor32(arg3.length)]
+    if not ext_call.success:
+        revert with ext_call.return_data[0 len return_data.size]
+    mem[96 len return_data.size] = ext_call.return_data[0 len return_data.size]
+    mem[64] = ceil32(return_data.size) + 96
+    require return_data.size >= 32
+    require mem[96 len 4], Mask(224, 32, arg2) >> 32 <= 4294967296
+    require mem[96 len 4], Mask(224, 32, arg2) >> 32 + 32 <= return_data.size
+    require mem[mem[96 len 4], Mask(224, 32, arg2) >> 32 + 96] <= 4294967296 and mem[96 len 4], Mask(224, 32, arg2) >> 32 + (32 * mem[mem[96 len 4], Mask(224, 32, arg2) >> 32 + 96]) + 32 <= return_data.size
+}
+
+function sub_9ff91014(?) payable {
+    require calldata.size - 4 >= 32
+    if owner != msg.sender:
+        revert with 0, 'Ownable: caller is not the owner'
+    if eth.balance(this.address) < 0:
+        revert with 0, 32, 38, 0x73416464726573733a20696e73756666696369656e742062616c616e636520666f722063616c, mem[366 len 26]
+    if ext_code.size(rewardTokenAddress) <= 0:
+        revert with 0, 'Address: call to non-contract'
+    mem[260 len 64] = unknown_0xa9059cbb(?????), Mask(224, 0, stor1), uint32(stor1), Mask(224, 32, arg1) >> 32
+    call rewardTokenAddress with:
+       funct uint32(stor1)
+         gas gas_remaining wei
+        args Mask(224, 32, arg1) << 224, mem[324 len 4]
+    if not return_data.size:
+        if not ext_call.success:
+            revert with unknown_0xa9059cbb(?????), Mask(224, 0, stor1), uint32(stor1), arg1
+        if not unknown_0xa9059cbb(?????), Mask(224, 0, stor1):
+            revert with 0, 32, 42, 0x6c5361666545524332303a204552433230206f7065726174696f6e20646964206e6f7420737563636565, mem[370 len 22]
+    else:
+        mem[292 len return_data.size] = ext_call.return_data[0 len return_data.size]
+        if not ext_call.success:
+            if return_data.size > 0:
+                revert with ext_call.return_data[0 len return_data.size]
+            revert with 0, 'SafeERC20: low-level call failed'
+        if return_data.size > 0:
+            require return_data.size >= 32
+            if not mem[292]:
+                revert with 0, 
+                            32,
+                            42,
+                            0x6c5361666545524332303a204552433230206f7065726174696f6e20646964206e6f7420737563636565,
+                            mem[ceil32(return_data.size) + 371 len 22]
+    require ext_code.size(address(stakingPoolAddress))
+    call address(stakingPoolAddress).notifyRewardAmount(uint256 rg1) with:
+         gas gas_remaining wei
+        args arg1
+    if not ext_call.success:
+        revert with ext_call.return_data[0 len return_data.size]
+}
+
+
+
+}
