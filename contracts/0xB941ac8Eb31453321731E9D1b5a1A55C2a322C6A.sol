6540eae5781f3e3c45a3b62aa8d9dae31508dcb7
bot <abagoforgans@users.noreply.github.com>
2022-06-26 21:41:41 +0000

    autocommit

---
+++ contracts/0xB941ac8Eb31453321731E9D1b5a1A55C2a322C6A.sol
@@ -0,0 +1,305 @@
+contract main {
+
+
+
+
+// =====================  Runtime code  =====================
+
+
+const name = 'TerraLuna'
+
+const decimals = 18
+
+const symbol = 'TLT'
+
+const sub_a4022bd1(?) = 0x197d4df19d605767337e9f14d3eec8920e400000000000000
+
+
+address owner;
+uint256 totalSupply;
+mapping of uint256 balanceOf;
+mapping of uint256 allowance;
+mapping of uint8 stor4;
+mapping of uint8 stor5;
+uint256 _taxFee;
+uint256 stor7;
+uint256 _burnFee;
+uint256 stor9;
+address projectAddress;
+address burnAddress;
+
+function totalSupply() {
+    return totalSupply
+}
+
+function _taxFee() {
+    return _taxFee
+}
+
+function projectAddress() {
+    return projectAddress
+}
+
+function isLocked(address arg1) {
+    return bool(stor5[address(arg1)])
+}
+
+function balanceOf(address arg1) {
+    return balanceOf[arg1]
+}
+
+function burnAddress() {
+    return burnAddress
+}
+
+function owner() {
+    return owner
+}
+
+function isExcludeFromFee(address arg1) {
+    return bool(stor4[address(arg1)])
+}
+
+function _burnFee() {
+    return _burnFee
+}
+
+function allowance(address arg1, address arg2) {
+    return allowance[arg1][arg2]
+}
+
+function _fallback() payable {
+    revert
+}
+
+function setTaxFee(uint256 arg1) {
+    require msg.sender == owner
+    _taxFee = arg1
+}
+
+function setBurnFee(uint256 arg1) {
+    require msg.sender == owner
+    _burnFee = arg1
+}
+
+function sub_9df788fc(?) {
+    require msg.sender == owner
+    stor5[address(arg1)] = 0
+}
+
+function setLocked(address arg1) {
+    require msg.sender == owner
+    stor5[address(arg1)] = 1
+}
+
+function includeInFee(address arg1) {
+    require msg.sender == owner
+    stor4[address(arg1)] = 0
+}
+
+function excludeFromFee(address arg1) {
+    require msg.sender == owner
+    stor4[address(arg1)] = 1
+}
+
+function setProjectAddress(address arg1) {
+    require msg.sender == owner
+    projectAddress = arg1
+}
+
+function renounceOwnership() {
+    require msg.sender == owner
+    emit OwnershipTransferred(owner, 0);
+    owner = 0
+}
+
+function transferOwnership(address arg1) {
+    require msg.sender == owner
+    require arg1
+    emit OwnershipTransferred(owner, arg1);
+    owner = arg1
+}
+
+function approve(address arg1, uint256 arg2) {
+    require arg1
+    allowance[msg.sender][address(arg1)] = arg2
+    emit Approval(arg2, msg.sender, arg1);
+    return 1
+}
+
+function decreaseAllowance(address arg1, uint256 arg2) {
+    require arg1
+    require arg2 <= allowance[msg.sender][address(arg1)]
+    allowance[msg.sender][address(arg1)] -= arg2
+    emit Approval((allowance[msg.sender][address(arg1)] - arg2), msg.sender, arg1);
+    return 1
+}
+
+function increaseAllowance(address arg1, uint256 arg2) {
+    require arg1
+    require arg2 + allowance[msg.sender][address(arg1)] >= allowance[msg.sender][address(arg1)]
+    allowance[msg.sender][address(arg1)] += arg2
+    emit Approval((arg2 + allowance[msg.sender][address(arg1)]), msg.sender, arg1);
+    return 1
+}
+
+function mintToken(address arg1, uint256 arg2) {
+    require msg.sender == owner
+    require arg2 + totalSupply <= 0x197d4df19d605767337e9f14d3eec8920e400000000000000
+    require arg2 + balanceOf[address(arg1)] >= balanceOf[address(arg1)]
+    balanceOf[address(arg1)] += arg2
+    require arg2 + totalSupply >= totalSupply
+    totalSupply += arg2
+    emit Transfer(arg2, 0, this.address);
+    emit Transfer(arg2, this.address, arg1);
+}
+
+function transfer(address arg1, uint256 arg2) {
+    if not arg1:
+        revert with 0, 'is 0 address'
+    if stor5[address(msg.sender)]:
+        revert with 0, 'is locked'
+    if stor4[address(msg.sender)]:
+        if _taxFee:
+            stor7 = _taxFee
+            stor9 = _burnFee
+            _taxFee = 0
+            _burnFee = 0
+        else:
+            if _burnFee:
+                stor7 = _taxFee
+                stor9 = _burnFee
+                _taxFee = 0
+                _burnFee = 0
+    if not arg2:
+        if not arg2:
+            require arg2 <= balanceOf[address(msg.sender)]
+            balanceOf[address(msg.sender)] -= arg2
+            require arg2 + balanceOf[arg1] >= balanceOf[arg1]
+            require 0 <= arg2 + balanceOf[arg1]
+            balanceOf[address(arg1)] = arg2 + balanceOf[arg1]
+        else:
+            require _burnFee * arg2 / arg2 == _burnFee
+            require arg2 <= balanceOf[address(msg.sender)]
+            balanceOf[address(msg.sender)] -= arg2
+            require arg2 + balanceOf[arg1] >= balanceOf[arg1]
+            require 0 <= arg2 + balanceOf[arg1]
+            require _burnFee * arg2 / 100 <= arg2 + balanceOf[arg1]
+            balanceOf[address(arg1)] = arg2 + balanceOf[arg1] - (_burnFee * arg2 / 100)
+            if _burnFee * arg2 / 100 > 0:
+                require (_burnFee * arg2 / 100) + balanceOf[stor11] >= balanceOf[stor11]
+                balanceOf[stor11] += _burnFee * arg2 / 100
+                emit Transfer((_burnFee * arg2 / 100), msg.sender, burnAddress);
+    else:
+        require _taxFee * arg2 / arg2 == _taxFee
+        if not arg2:
+            require arg2 <= balanceOf[address(msg.sender)]
+            balanceOf[address(msg.sender)] -= arg2
+            require arg2 + balanceOf[arg1] >= balanceOf[arg1]
+            require _taxFee * arg2 / 100 <= arg2 + balanceOf[arg1]
+            require 0 <= arg2 + balanceOf[arg1] - (_taxFee * arg2 / 100)
+            balanceOf[address(arg1)] = arg2 + balanceOf[arg1] - (_taxFee * arg2 / 100)
+            if _taxFee * arg2 / 100 > 0:
+                require (_taxFee * arg2 / 100) + balanceOf[stor10] >= balanceOf[stor10]
+                balanceOf[stor10] += _taxFee * arg2 / 100
+                emit Transfer((_taxFee * arg2 / 100), msg.sender, projectAddress);
+        else:
+            require _burnFee * arg2 / arg2 == _burnFee
+            require arg2 <= balanceOf[address(msg.sender)]
+            balanceOf[address(msg.sender)] -= arg2
+            require arg2 + balanceOf[arg1] >= balanceOf[arg1]
+            require _taxFee * arg2 / 100 <= arg2 + balanceOf[arg1]
+            require _burnFee * arg2 / 100 <= arg2 + balanceOf[arg1] - (_taxFee * arg2 / 100)
+            balanceOf[address(arg1)] = arg2 + balanceOf[arg1] - (_taxFee * arg2 / 100) - (_burnFee * arg2 / 100)
+            if _taxFee * arg2 / 100 > 0:
+                require (_taxFee * arg2 / 100) + balanceOf[stor10] >= balanceOf[stor10]
+                balanceOf[stor10] += _taxFee * arg2 / 100
+                emit Transfer((_taxFee * arg2 / 100), msg.sender, projectAddress);
+            if _burnFee * arg2 / 100 > 0:
+                require (_burnFee * arg2 / 100) + balanceOf[stor11] >= balanceOf[stor11]
+                balanceOf[stor11] += _burnFee * arg2 / 100
+                emit Transfer((_burnFee * arg2 / 100), msg.sender, burnAddress);
+    if stor4[address(msg.sender)]:
+        _taxFee = stor7
+        _burnFee = stor9
+    emit Transfer(arg2, msg.sender, arg1);
+    return 1
+}
+
+function transferFrom(address arg1, address arg2, uint256 arg3) {
+    require arg3 <= allowance[address(arg1)][msg.sender]
+    allowance[address(arg1)][msg.sender] -= arg3
+    if not arg2:
+        revert with 0, 'is 0 address'
+    if stor5[address(arg1)]:
+        revert with 0, 'is locked'
+    if stor4[address(arg1)]:
+        if _taxFee:
+            stor7 = _taxFee
+            stor9 = _burnFee
+            _taxFee = 0
+            _burnFee = 0
+        else:
+            if _burnFee:
+                stor7 = _taxFee
+                stor9 = _burnFee
+                _taxFee = 0
+                _burnFee = 0
+    if not arg3:
+        if not arg3:
+            require arg3 <= balanceOf[address(arg1)]
+            balanceOf[address(arg1)] -= arg3
+            require arg3 + balanceOf[arg2] >= balanceOf[arg2]
+            require 0 <= arg3 + balanceOf[arg2]
+            balanceOf[address(arg2)] = arg3 + balanceOf[arg2]
+        else:
+            require _burnFee * arg3 / arg3 == _burnFee
+            require arg3 <= balanceOf[address(arg1)]
+            balanceOf[address(arg1)] -= arg3
+            require arg3 + balanceOf[arg2] >= balanceOf[arg2]
+            require 0 <= arg3 + balanceOf[arg2]
+            require _burnFee * arg3 / 100 <= arg3 + balanceOf[arg2]
+            balanceOf[address(arg2)] = arg3 + balanceOf[arg2] - (_burnFee * arg3 / 100)
+            if _burnFee * arg3 / 100 > 0:
+                require (_burnFee * arg3 / 100) + balanceOf[stor11] >= balanceOf[stor11]
+                balanceOf[stor11] += _burnFee * arg3 / 100
+                emit Transfer((_burnFee * arg3 / 100), arg1, burnAddress);
+    else:
+        require _taxFee * arg3 / arg3 == _taxFee
+        if not arg3:
+            require arg3 <= balanceOf[address(arg1)]
+            balanceOf[address(arg1)] -= arg3
+            require arg3 + balanceOf[arg2] >= balanceOf[arg2]
+            require _taxFee * arg3 / 100 <= arg3 + balanceOf[arg2]
+            require 0 <= arg3 + balanceOf[arg2] - (_taxFee * arg3 / 100)
+            balanceOf[address(arg2)] = arg3 + balanceOf[arg2] - (_taxFee * arg3 / 100)
+            if _taxFee * arg3 / 100 > 0:
+                require (_taxFee * arg3 / 100) + balanceOf[stor10] >= balanceOf[stor10]
+                balanceOf[stor10] += _taxFee * arg3 / 100
+                emit Transfer((_taxFee * arg3 / 100), arg1, projectAddress);
+        else:
+            require _burnFee * arg3 / arg3 == _burnFee
+            require arg3 <= balanceOf[address(arg1)]
+            balanceOf[address(arg1)] -= arg3
+            require arg3 + balanceOf[arg2] >= balanceOf[arg2]
+            require _taxFee * arg3 / 100 <= arg3 + balanceOf[arg2]
+            require _burnFee * arg3 / 100 <= arg3 + balanceOf[arg2] - (_taxFee * arg3 / 100)
+            balanceOf[address(arg2)] = arg3 + balanceOf[arg2] - (_taxFee * arg3 / 100) - (_burnFee * arg3 / 100)
+            if _taxFee * arg3 / 100 > 0:
+                require (_taxFee * arg3 / 100) + balanceOf[stor10] >= balanceOf[stor10]
+                balanceOf[stor10] += _taxFee * arg3 / 100
+                emit Transfer((_taxFee * arg3 / 100), arg1, projectAddress);
+            if _burnFee * arg3 / 100 > 0:
+                require (_burnFee * arg3 / 100) + balanceOf[stor11] >= balanceOf[stor11]
+                balanceOf[stor11] += _burnFee * arg3 / 100
+                emit Transfer((_burnFee * arg3 / 100), arg1, burnAddress);
+    if stor4[address(arg1)]:
+        _taxFee = stor7
+        _burnFee = stor9
+    emit Transfer(arg3, arg1, arg2);
+    return 1
+}
+
+
+
+}
---
+++ contracts/0xFB601531864FeE94Ca4329F4232231Fb880A56b1.sol
@@ -0,0 +1,374 @@
+contract main {
+
+
+
+
+// =====================  Runtime code  =====================
+
+
+#
+#  - transferFrom(address arg1, address arg2, uint256 arg3)
+#  - transfer(address arg1, uint256 arg2)
+#  - _fallback()
+#
+address _owner;
+mapping of uint256 stor1;
+mapping of uint256 allowance;
+mapping of uint8 stor4;
+uint256 totalSupply;
+uint256 stor6;
+uint256 totalFees;
+array of struct stor9;
+array of struct stor10;
+uint256 decimals;
+uint256 _taxFee;
+uint256 _destroyFee;
+uint256 sub_a41a08fb;
+address sub_b1e1fd4bAddress;
+mapping of address inviter;
+mapping of uint256 lastSellTime;
+address uniswapV2PairAddress;
+address sub_e483a254Address;
+uint256 sub_885b8716;
+
+function totalFees() {
+    return totalFees
+}
+
+function totalSupply() {
+    return totalSupply
+}
+
+function decimals() {
+    return decimals
+}
+
+function _taxFee() {
+    return _taxFee
+}
+
+function uniswapV2Pair() {
+    return uniswapV2PairAddress
+}
+
+function isExcludedFromFee(address arg1) {
+    require calldata.size - 4 >= 32
+    require arg1 == arg1
+    return bool(stor4[address(arg1)])
+}
+
+function sub_885b8716(?) {
+    return sub_885b8716
+}
+
+function owner() {
+    return _owner
+}
+
+function _destroyFee() {
+    return _destroyFee
+}
+
+function sub_a41a08fb(?) {
+    return sub_a41a08fb
+}
+
+function sub_b1e1fd4b(?) {
+    return sub_b1e1fd4bAddress
+}
+
+function _owner() {
+    return _owner
+}
+
+function lastSellTime(address arg1) {
+    require calldata.size - 4 >= 32
+    require arg1 == arg1
+    return lastSellTime[arg1]
+}
+
+function allowance(address arg1, address arg2) {
+    require calldata.size - 4 >= 64
+    require arg1 == arg1
+    require arg2 == arg2
+    return allowance[address(arg1)][address(arg2)]
+}
+
+function sub_e483a254(?) {
+    return sub_e483a254Address
+}
+
+function inviter(address arg1) {
+    require calldata.size - 4 >= 32
+    require arg1 == arg1
+    return inviter[arg1]
+}
+
+function changeOwner(address arg1) {
+    require calldata.size - 4 >= 32
+    require arg1 == arg1
+    if _owner != msg.sender:
+        revert with 0, 'Ownable: caller is not the owner'
+    _owner = arg1
+}
+
+function includeInFee(address arg1) {
+    require calldata.size - 4 >= 32
+    require arg1 == arg1
+    if _owner != msg.sender:
+        revert with 0, 'Ownable: caller is not the owner'
+    stor4[address(arg1)] = 0
+}
+
+function excludeFromFee(address arg1) {
+    require calldata.size - 4 >= 32
+    require arg1 == arg1
+    if _owner != msg.sender:
+        revert with 0, 'Ownable: caller is not the owner'
+    stor4[address(arg1)] = 1
+}
+
+function changeRouter(address arg1) {
+    require calldata.size - 4 >= 32
+    require arg1 == arg1
+    if _owner != msg.sender:
+        revert with 0, 'Ownable: caller is not the owner'
+    uniswapV2PairAddress = arg1
+}
+
+function claimTokens() {
+    if _owner != msg.sender:
+        revert with 0, 'Ownable: caller is not the owner'
+    call _owner with:
+       value eth.balance(this.address) wei
+         gas 2300 * is_zero(value) wei
+    if not ext_call.success:
+        revert with ext_call.return_data[0 len return_data.size]
+}
+
+function approve(address arg1, uint256 arg2) {
+    require calldata.size - 4 >= 64
+    require arg1 == arg1
+    if not msg.sender:
+        revert with 0x8c379a000000000000000000000000000000000000000000000000000000000, 'ERC20: approve from the zero address'
+    if not arg1:
+        revert with 0x8c379a000000000000000000000000000000000000000000000000000000000, 'ERC20: approve to the zero address'
+    allowance[address(msg.sender)][address(arg1)] = arg2
+    emit Approval(arg2, msg.sender, arg1);
+    return 1
+}
+
+function tokenFromReflection(uint256 arg1) {
+    require calldata.size - 4 >= 32
+    if arg1 > stor6:
+        revert with 0x8c379a000000000000000000000000000000000000000000000000000000000, 'Amount must be less than total reflections'
+    if not totalSupply:
+        revert with 0, 'SafeMath: division by zero', 0
+    if not totalSupply:
+        revert with 0, 'SafeMath: division by zero', 0
+    if not stor6 / totalSupply:
+        revert with 0, 'SafeMath: division by zero', 0
+    return (arg1 / stor6 / totalSupply)
+}
+
+function balanceOf(address arg1) {
+    require calldata.size - 4 >= 32
+    require arg1 == arg1
+    if stor1[address(arg1)] > stor6:
+        revert with 0x8c379a000000000000000000000000000000000000000000000000000000000, 'Amount must be less than total reflections'
+    if not totalSupply:
+        revert with 0, 'SafeMath: division by zero', 0
+    if not totalSupply:
+        revert with 0, 'SafeMath: division by zero', 0
+    if not stor6 / totalSupply:
+        revert with 0, 'SafeMath: division by zero', 0
+    return (stor1[address(arg1)] / stor6 / totalSupply)
+}
+
+function decreaseAllowance(address arg1, uint256 arg2) {
+    require calldata.size - 4 >= 64
+    require arg1 == arg1
+    if arg2 > allowance[msg.sender][address(arg1)]:
+        revert with 0, 32, 37, 0x6545524332303a2064656372656173656420616c6c6f77616e63652062656c6f77207a6572, mem[165 len 27] >> 40, 0
+    if allowance[msg.sender][address(arg1)] < arg2:
+        revert with 0, 17
+    if not msg.sender:
+        revert with 0, 'ERC20: approve from the zero address'
+    if not arg1:
+        revert with 0, 'ERC20: approve to the zero address'
+    allowance[address(msg.sender)][address(arg1)] = allowance[msg.sender][address(arg1)] - arg2
+    emit Approval((allowance[msg.sender][address(arg1)] - arg2), msg.sender, arg1);
+    return 1
+}
+
+function increaseAllowance(address arg1, uint256 arg2) {
+    require calldata.size - 4 >= 64
+    require arg1 == arg1
+    if allowance[msg.sender][address(arg1)] > !arg2:
+        revert with 0, 17
+    if allowance[msg.sender][address(arg1)] + arg2 < allowance[msg.sender][address(arg1)]:
+        revert with 0, 'SafeMath: addition overflow'
+    if not msg.sender:
+        revert with 0x8c379a000000000000000000000000000000000000000000000000000000000, 'ERC20: approve from the zero address'
+    if not arg1:
+        revert with 0x8c379a000000000000000000000000000000000000000000000000000000000, 'ERC20: approve to the zero address'
+    allowance[address(msg.sender)][address(arg1)] = allowance[msg.sender][address(arg1)] + arg2
+    emit Approval((allowance[msg.sender][address(arg1)] + arg2), msg.sender, arg1);
+    return 1
+}
+
+function name() {
+    if bool(stor9.length):
+        if bool(stor9.length) == uint255(stor9.length) * 0.5 < 32:
+            revert with 0, 34
+        if bool(stor9.length):
+            if bool(stor9.length) == uint255(stor9.length) * 0.5 < 32:
+                revert with 0, 34
+            if Mask(256, -1, stor9.length):
+                if 31 < uint255(stor9.length) * 0.5:
+                    mem[128] = uint256(stor9.field_0)
+                    idx = 128
+                    s = 0
+                    while (uint255(stor9.length) * 0.5) + 96 > idx:
+                        mem[idx + 32] = stor9[s].field_256
+                        idx = idx + 32
+                        s = s + 1
+                        continue 
+                    return Array(len=2 * Mask(256, -1, stor9.length), data=mem[128 len ceil32(uint255(stor9.length) * 0.5)])
+                mem[128] = 256 * stor9.length.field_8
+        else:
+            if bool(stor9.length) == stor9.length.field_1 < 32:
+                revert with 0, 34
+            if stor9.length.field_1:
+                if 31 < stor9.length.field_1:
+                    mem[128] = uint256(stor9.field_0)
+                    idx = 128
+                    s = 0
+                    while stor9.length.field_1 + 96 > idx:
+                        mem[idx + 32] = stor9[s].field_256
+                        idx = idx + 32
+                        s = s + 1
+                        continue 
+                    return Array(len=2 * Mask(256, -1, stor9.length), data=mem[128 len ceil32(uint255(stor9.length) * 0.5)])
+                mem[128] = 256 * stor9.length.field_8
+        mem[ceil32(uint255(stor9.length) * 0.5) + 192 len ceil32(uint255(stor9.length) * 0.5)] = mem[128 len ceil32(uint255(stor9.length) * 0.5)]
+        if ceil32(uint255(stor9.length) * 0.5) > uint255(stor9.length) * 0.5:
+            mem[ceil32(uint255(stor9.length) * 0.5) + (uint255(stor9.length) * 0.5) + 192] = 0
+        return Array(len=2 * Mask(256, -1, stor9.length), data=mem[128 len ceil32(uint255(stor9.length) * 0.5)], mem[(2 * ceil32(uint255(stor9.length) * 0.5)) + 192 len 2 * ceil32(uint255(stor9.length) * 0.5)]), 
+    if bool(stor9.length) == stor9.length.field_1 < 32:
+        revert with 0, 34
+    if bool(stor9.length):
+        if bool(stor9.length) == uint255(stor9.length) * 0.5 < 32:
+            revert with 0, 34
+        if Mask(256, -1, stor9.length):
+            if 31 < uint255(stor9.length) * 0.5:
+                mem[128] = uint256(stor9.field_0)
+                idx = 128
+                s = 0
+                while (uint255(stor9.length) * 0.5) + 96 > idx:
+                    mem[idx + 32] = stor9[s].field_256
+                    idx = idx + 32
+                    s = s + 1
+                    continue 
+                return Array(len=stor9.length % 128, data=mem[128 len ceil32(stor9.length.field_1)])
+            mem[128] = 256 * stor9.length.field_8
+    else:
+        if bool(stor9.length) == stor9.length.field_1 < 32:
+            revert with 0, 34
+        if stor9.length.field_1:
+            if 31 < stor9.length.field_1:
+                mem[128] = uint256(stor9.field_0)
+                idx = 128
+                s = 0
+                while stor9.length.field_1 + 96 > idx:
+                    mem[idx + 32] = stor9[s].field_256
+                    idx = idx + 32
+                    s = s + 1
+                    continue 
+                return Array(len=stor9.length % 128, data=mem[128 len ceil32(stor9.length.field_1)])
+            mem[128] = 256 * stor9.length.field_8
+    mem[ceil32(stor9.length.field_1) + 192 len ceil32(stor9.length.field_1)] = mem[128 len ceil32(stor9.length.field_1)]
+    if ceil32(stor9.length.field_1) > stor9.length.field_1:
+        mem[ceil32(stor9.length.field_1) + stor9.length.field_1 + 192] = 0
+    return Array(len=stor9.length % 128, data=mem[128 len ceil32(stor9.length.field_1)], mem[(2 * ceil32(stor9.length.field_1)) + 192 len 2 * ceil32(stor9.length.field_1)]), 
+}
+
+function symbol() {
+    if bool(stor10.length):
+        if bool(stor10.length) == uint255(stor10.length) * 0.5 < 32:
+            revert with 0, 34
+        if bool(stor10.length):
+            if bool(stor10.length) == uint255(stor10.length) * 0.5 < 32:
+                revert with 0, 34
+            if Mask(256, -1, stor10.length):
+                if 31 < uint255(stor10.length) * 0.5:
+                    mem[128] = uint256(stor10.field_0)
+                    idx = 128
+                    s = 0
+                    while (uint255(stor10.length) * 0.5) + 96 > idx:
+                        mem[idx + 32] = stor10[s].field_256
+                        idx = idx + 32
+                        s = s + 1
+                        continue 
+                    return Array(len=2 * Mask(256, -1, stor10.length), data=mem[128 len ceil32(uint255(stor10.length) * 0.5)])
+                mem[128] = 256 * stor10.length.field_8
+        else:
+            if bool(stor10.length) == stor10.length.field_1 < 32:
+                revert with 0, 34
+            if stor10.length.field_1:
+                if 31 < stor10.length.field_1:
+                    mem[128] = uint256(stor10.field_0)
+                    idx = 128
+                    s = 0
+                    while stor10.length.field_1 + 96 > idx:
+                        mem[idx + 32] = stor10[s].field_256
+                        idx = idx + 32
+                        s = s + 1
+                        continue 
+                    return Array(len=2 * Mask(256, -1, stor10.length), data=mem[128 len ceil32(uint255(stor10.length) * 0.5)])
+                mem[128] = 256 * stor10.length.field_8
+        mem[ceil32(uint255(stor10.length) * 0.5) + 192 len ceil32(uint255(stor10.length) * 0.5)] = mem[128 len ceil32(uint255(stor10.length) * 0.5)]
+        if ceil32(uint255(stor10.length) * 0.5) > uint255(stor10.length) * 0.5:
+            mem[ceil32(uint255(stor10.length) * 0.5) + (uint255(stor10.length) * 0.5) + 192] = 0
+        return Array(len=2 * Mask(256, -1, stor10.length), data=mem[128 len ceil32(uint255(stor10.length) * 0.5)], mem[(2 * ceil32(uint255(stor10.length) * 0.5)) + 192 len 2 * ceil32(uint255(stor10.length) * 0.5)]), 
+    if bool(stor10.length) == stor10.length.field_1 < 32:
+        revert with 0, 34
+    if bool(stor10.length):
+        if bool(stor10.length) == uint255(stor10.length) * 0.5 < 32:
+            revert with 0, 34
+        if Mask(256, -1, stor10.length):
+            if 31 < uint255(stor10.length) * 0.5:
+                mem[128] = uint256(stor10.field_0)
+                idx = 128
+                s = 0
+                while (uint255(stor10.length) * 0.5) + 96 > idx:
+                    mem[idx + 32] = stor10[s].field_256
+                    idx = idx + 32
+                    s = s + 1
+                    continue 
+                return Array(len=stor10.length % 128, data=mem[128 len ceil32(stor10.length.field_1)])
+            mem[128] = 256 * stor10.length.field_8
+    else:
+        if bool(stor10.length) == stor10.length.field_1 < 32:
+            revert with 0, 34
+        if stor10.length.field_1:
+            if 31 < stor10.length.field_1:
+                mem[128] = uint256(stor10.field_0)
+                idx = 128
+                s = 0
+                while stor10.length.field_1 + 96 > idx:
+                    mem[idx + 32] = stor10[s].field_256
+                    idx = idx + 32
+                    s = s + 1
+                    continue 
+                return Array(len=stor10.length % 128, data=mem[128 len ceil32(stor10.length.field_1)])
+            mem[128] = 256 * stor10.length.field_8
+    mem[ceil32(stor10.length.field_1) + 192 len ceil32(stor10.length.field_1)] = mem[128 len ceil32(stor10.length.field_1)]
+    if ceil32(stor10.length.field_1) > stor10.length.field_1:
+        mem[ceil32(stor10.length.field_1) + stor10.length.field_1 + 192] = 0
+    return Array(len=stor10.length % 128, data=mem[128 len ceil32(stor10.length.field_1)], mem[(2 * ceil32(stor10.length.field_1)) + 192 len 2 * ceil32(stor10.length.field_1)]), 
+}
+
+
+
+}
